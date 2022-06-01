8c1f24af086a52f2bdb977c595f4b8ea32bab58b
Tatyana Atanasova <106607660+tatianna10@users.noreply.github.com>
2022-06-01 10:34:59 +0300

    Update streamlit_app.py
    emojis fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healty Diner')
 
-streamlit.header('Breakfast Favorites')
-streamlit.text(' Omega 3 & Blueberry Oatmeal')
-streamlit.text(' (3 Kale, Spinach & Rocket Smoothie')
-streamlit.text('4 Hard-Boiled Free-Range Egg')
-streamlit.text('OO Avocado Toast')
+streamlit.header('Breakfast Favorites') 
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
