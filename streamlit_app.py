a41adeb3e7bd1346814867885f28126bd47af8ec
Tatyana Atanasova <106607660+tatianna10@users.noreply.github.com>
2022-06-01 10:30:54 +0300

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healty Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text(' Omega 3 & Blueberry Oatmeal')
-streamlit.text(' (3 Kale, Spinach & Rocket Smoothie'
-streamlit.text('4 Hard-Boiled Free-Range Egg'
+streamlit.text(' (3 Kale, Spinach & Rocket Smoothie')
+streamlit.text('4 Hard-Boiled Free-Range Egg')
 streamlit.text('OO Avocado Toast')
